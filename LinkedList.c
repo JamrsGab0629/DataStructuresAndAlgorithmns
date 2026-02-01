0158f50e0f092594f33cfbcd1d89c9fc41488072
JamrsGab0629 <jamesgabrielsantos0629@gmail.com>
2026-02-01 13:21:54 +0800

    linkedl list

--- LinkedList.c
+++ LinkedList.c
@@ -1,16 +1,51 @@
-    #include <stdio.h>
-    void swap(int *a,int *b){
-        int temp = *a;
-        *a = *b;
-        *b = temp;
+#include <stdio.h>
+#include <stdlib.h>
 
-        
-    }
-    int main(){
-int h = 10;
-int j = 5;
-printf("%d,%d\n",h,j);
-swap(&h,&j);
-printf("%d,%d",h,j);
-    }
 
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+
+struct Node *createnode(int value){
+    struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
+   
+    if(newnode == NULL){
+        printf("memory allocation failed");
+        return NULL;
+    }
+    newnode->data = value;
+    newnode->next = NULL;
+    return newnode;
+}
+void InsertatEnd(struct Node **head,int value){
+    struct Node *newnode = createnode(value);
+    if(*head == NULL){
+       *head = newnode;
+       return;
+    }
+    struct Node *temp = *head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newnode;
+}
+
+int main(){
+  
+struct Node *head = NULL;
+
+InsertatEnd(&head,10);
+InsertatEnd(&head,12);
+InsertatEnd(&head,70);
+
+
+struct Node *temp = head;
+while(temp != NULL){
+    printf("%d->",temp->data);
+    temp = temp->next;
+}
+
+    
+}
